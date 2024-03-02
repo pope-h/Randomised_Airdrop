b84ce7114857554a59fad1f9fa6d8fc16533c7fc
Ekarika Nsemeke <hamatthan@gmail.com>
2024-03-02 03:03:34 +0100

    verified the contract

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,15 +2,18 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 require("dotenv").config();
 
-const { URL, PRIVATE_KEY } = process.env;
+const { URL, PRIVATE_KEY, ETHERSCAN_API_KEY } = process.env;
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
-   networks: {
+  networks: {
     sepolia: {
       url: URL,
       accounts: [`0x${PRIVATE_KEY}`],
     },
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
   },
 };
 
